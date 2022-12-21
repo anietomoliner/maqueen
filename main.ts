5e6574904248336bef21a00f871be32851660ff6
anietomoliner <121170490+anietomoliner@users.noreply.github.com>
2022-12-21 19:05:41 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,26 @@
 let Distancia = maqueen.Ultrasonic(PingUnit.Centimeters)
+pins.digitalWritePin(DigitalPin.P12, 1)
+pins.digitalWritePin(DigitalPin.P8, 1)
+basic.showNumber(Distancia)
 basic.forever(function () {
+    Distancia = maqueen.Ultrasonic(PingUnit.Centimeters)
     if (Distancia > 5) {
-        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) < 150) {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) < 255) {
             pins.digitalWritePin(DigitalPin.P8, 1)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 154)
         } else {
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
             pins.digitalWritePin(DigitalPin.P8, 0)
         }
-        if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) < 150) {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) < 255) {
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 154)
         } else {
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
             pins.digitalWritePin(DigitalPin.P12, 1)
         }
         pins.digitalWritePin(DigitalPin.P12, 0)
+    } else {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
     }
 })
