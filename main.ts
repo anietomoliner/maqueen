da5a721f0a4deb4ec279a319a4f914e516cadf81
anietomoliner <121170490+anietomoliner@users.noreply.github.com>
2022-12-21 18:01:37 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,9 +3,13 @@ basic.forever(function () {
     if (Distancia > 5) {
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) > 150) {
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 154)
+        } else {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
         }
         if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) > 150) {
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 154)
+        } else {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
         }
     }
 })
