a1d4452f4e93b39b8f607bf00933b5969340f5d2
anietomoliner <121170490+anietomoliner@users.noreply.github.com>
2022-12-21 19:18:08 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,14 +5,14 @@ basic.showNumber(3)
 basic.forever(function () {
     Distancia = Math.round(maqueen.Ultrasonic(PingUnit.Centimeters))
     if (Distancia > 5) {
-        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) < 255) {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
             pins.digitalWritePin(DigitalPin.P8, 1)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 154)
         } else {
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
             pins.digitalWritePin(DigitalPin.P8, 0)
         }
-        if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) < 255) {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 154)
         } else {
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
